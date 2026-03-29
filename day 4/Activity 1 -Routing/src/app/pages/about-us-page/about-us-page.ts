35c8ffe8ccff91feb47110439f1b077b023e3418
Osagani31 <naonperera@students.nsbm.ac.lk>
2026-03-29 11:11:57 +0530

    Delete day 4/Activity 1 -Routing/src directory

--- day 4/Activity 1 -Routing/src/app/pages/about-us-page/about-us-page.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-about-us-page',
-  imports: [],
-  templateUrl: './about-us-page.html',
-  styleUrl: './about-us-page.scss',
-})
-export class AboutUsPage {}
